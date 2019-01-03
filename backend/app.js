5fd67fe6acdea28766fdd63eb37437aaf45f8431
Gerd Schommers <gerd@marie-luce.be>
2019-01-03 15:25:16 +0100

    init backend

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(morgan('short'));
+app.use(cors());
+
+
+const router = require('./routes/');
+app.use(router);
+
+app.listen(4040, () =>{
+    console.log("API is running on port 4040...")
+});
