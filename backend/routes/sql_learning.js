b9da13ae1ddc31a7346b68cee1a8036118c2bad8
Gerd Schommers <gerd@marie-luce.be>
2019-01-04 11:31:12 +0100

    route select all personnes

--- backend/routes/sql_learning.js
+++ backend/routes/sql_learning.js
@@ -3,22 +3,38 @@ const mysql = require('mysql');
 const router = express.Router();
 
 router.get('/facile', (req, res)=> {
-    console.log("Fetching table 'facile'...");
+    console.log("Fetching table 'personnes'...");
 
-    const getAll = "SELECT * FROM facile";
+    const getAll = "SELECT * FROM personnes";
     getConnection().query(getAll, (err, rows, fields) => {
         if (err) {
             console.log("Failed to getAll: " + err);
             res.sendStatus(500);
             return
         }
-
         res.json(rows)
     })
 });
 
-router.get('/facileTest', (req, res) => {
+router.get('/facileMotsCles/:exercice', (req, res)=> {
+    console.log("Fetching keywords for exercice 'facile'...");
+
+    const exercice = req.params.id;
+    const requete = 'SELECT * FROM motscles where exercice =' + mysql.escape(exercice);
+    getConnection().query(requete, (err, rows, fields) => {
+        if (err) {
+            console.log("Failed to getAll: " + err);
+            res.sendStatus(500);
+            return
+        }
+        res.json(rows)
+    })
+});
+
+router.get('/facileSqlRequest', (req, res) => {
     console.log("trying 'sqlFacile request'... ");
+    // tests à effectuer :
+
     getConnection().query(req, [legumeId], (err, rows, fields) => {
         if (err) {
             console.log("Failed to execute request " + err);
