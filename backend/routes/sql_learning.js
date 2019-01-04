c576a065751d422662ab2a080d3064455d58b076
Gerd Schommers <gerd@marie-luce.be>
2019-01-04 14:03:00 +0100

    requete variable

--- backend/routes/sql_learning.js
+++ backend/routes/sql_learning.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express'); // router framework for node.js
 const mysql = require('mysql');
 const router = express.Router();
 
@@ -17,9 +17,8 @@ router.get('/easy', (req, res)=> {
 });
 
 router.get('/easyExercice/:id', (req, res)=> {
-    console.log("Fetching text for exercice 'facile'...");
+    console.log("Fetching text for exercice " + req.params.id);
     const exercice = req.params.id;
-    console.log(exercice);
     const requete = 'SELECT * FROM exercices where exercice =' + mysql.escape(exercice);
     // const requete = 'SELECT * FROM motscles';
     getConnection().query(requete, (err, rows, fields) => {
@@ -35,9 +34,7 @@ router.get('/easyExercice/:id', (req, res)=> {
 router.get('/easyMotsCles/:id', (req, res)=> {
     console.log("Fetching keywords for exercice 'facile'...");
     const exercice = req.params.id;
-    console.log(exercice);
     const requete = 'SELECT * FROM motscles where exercice =' + mysql.escape(exercice);
-    // const requete = 'SELECT * FROM motscles';
     getConnection().query(requete, (err, rows, fields) => {
         if (err) {
             console.log("Failed to getAll: " + err);
@@ -50,16 +47,36 @@ router.get('/easyMotsCles/:id', (req, res)=> {
 
 router.get('/easySqlRequest', (req, res) => {
     console.log("trying 'sqlFacile request'... ");
-    // tests à effectuer :
-
-    getConnection().query(req, [legumeId], (err, rows, fields) => {
+    const requete = req.query.q;
+    console.log(requete);
+    getConnection().query(requete, (err, rows, fields) => {
         if (err) {
-            console.log("Failed to execute request " + err);
+            console.log("Failed to getAll: " + err);
             res.sendStatus(500);
             return
         }
+        res.json(rows);
 
-        res.json(rows)
+        console.log(rows[1].motcle);
+    })
+});
+
+router.get('/easySqlRequest/:id', (req, res) => {
+    console.log("trying 'sqlFacile request'... ");
+    // récupérer les mots cles de l'exercice :
+    const exercice = req.params.id;
+    const requete = 'SELECT motcle FROM motscles where exercice =' + mysql.escape(exercice);
+    getConnection().query(requete, (err, rows, fields) => {
+        if (err) {
+            console.log("Failed to getAll: " + err);
+            res.sendStatus(500);
+            return
+        }
+        res.json(rows);
+
+        for (var i=0;i<rows.length;i++){
+            console.log(rows[i].motcle);
+        }
     })
 });
 
